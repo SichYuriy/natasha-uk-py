dfab3548a599c990ed1c8df9af2c6882cf21eafb
Yurii Sych <sichyuriyy@gmail.com>
2019-09-12 21:49:19 +0300

    add uk person extracting service

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,2 @@
 from .name_extracting_service import name_extracting_service
+from .person_extracting_service import person_extracting_service
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import request
 
 import json
 from api import name_extracting_service
+from api import person_extracting_service
 
 app = Flask(__name__)
 
@@ -12,6 +13,14 @@ app = Flask(__name__)
 def extract_names_uk():
     articles = json.loads(request.data)
     articles_matches = name_extracting_service.extract_names(articles)
+    dto = list(map(lambda matches: matches.as_json, articles_matches))
+    return Response(json.dumps(dto), mimetype='application/json')
+
+
+@app.route('/extract-persons-uk', methods=['POST'])
+def extract_persons_uk():
+    articles = json.loads(request.data)
+    articles_matches = person_extracting_service.extract_persons(articles)
     dto = list(map(lambda matches: matches.as_json, articles_matches))
     return Response(json.dumps(dto), mimetype='application/json')
 
