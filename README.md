36d55f7a46d35dea050735ee483cf0020c45f5e2
Yurii Sych <sichyuriyy@gmail.com>
2019-10-06 21:30:28 +0300

    removed hardcoded db url and port

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
  - python -m venv venv
  - venv\Scripts\pip.exe install -r requirements.txt
 
-## run on port 8100
+## run on port 8100 with local db
  - set FLASK_RUN_PORT=8100
+ - set NATASHA_MONGO_DB_HOST_URL=localhost
+ - set NATASHA_MONGO_DB_PORT=27017
  - venv\Scripts\python.exe -m flask run
--- vesum/vesum_service.py
+++ vesum/vesum_service.py
@@ -1,11 +1,14 @@
 from pymongo import MongoClient
+import os
 
-client = MongoClient('localhost', 27017, maxPoolSize=20)
+mongoDbHost = ('NATASHA_MONGO_DB_HOST_URL' in os.environ and os.environ['NATASHA_MONGO_DB_HOST_URL']) or 'localhost'
+mongoDbPort = ('NATASHA_MONGO_DB_PORT' in os.environ and int(os.environ['NATASHA_MONGO_DB_PORT'])) or 27017
+client = MongoClient(mongoDbHost, mongoDbPort, maxPoolSize=20)
 
 
 class VesumService():
     def findByWordForm(self, wordForm):
-        return client['diploma-sb-database']['vesumentry'].find({'word': wordForm})
+        return client['natasha_uk_database']['vesumentry'].find({'word': wordForm})
 
 
 vesum_service = VesumService()
